0eebef8e39cfba52a583cf74e1373939f0be3e78
ViraMesha <wira.mescha@gmail.com>
2023-10-19 01:06:03 +0300

    feat: initial commit

---
+++ lib/data.ts
@@ -0,0 +1,107 @@
+import React from "react";
+import { CgWorkAlt } from "react-icons/cg";
+import { FaReact } from "react-icons/fa";
+import { LuGraduationCap } from "react-icons/lu";
+import valwaxImg from "@/public/valwax.png";
+import yourPetImg from "@/public/yourPet.png";
+import remoteDeminingImg from "@/public/remoteDemining.png";
+
+export const links = [
+  {
+    name: "Home",
+    hash: "#home",
+  },
+  {
+    name: "About",
+    hash: "#about",
+  },
+  {
+    name: "Projects",
+    hash: "#projects",
+  },
+  {
+    name: "Skills",
+    hash: "#skills",
+  },
+  {
+    name: "Experience",
+    hash: "#experience",
+  },
+  {
+    name: "Contact",
+    hash: "#contact",
+  },
+] as const;
+
+export const experiencesData = [
+  {
+    title: "Graduated bootcamp",
+    location: "Miami, FL",
+    description:
+      "I graduated after 6 months of studying. I immediately found a job as a front-end developer.",
+    icon: React.createElement(LuGraduationCap),
+    date: "2019",
+  },
+  {
+    title: "Front-End Developer",
+    location: "Orlando, FL",
+    description:
+      "I worked as a front-end developer for 2 years in 1 job and 1 year in another job. I also upskilled to the full stack.",
+    icon: React.createElement(CgWorkAlt),
+    date: "2019 - 2021",
+  },
+  {
+    title: "Full-Stack Developer",
+    location: "Houston, TX",
+    description:
+      "I'm now a full-stack developer working as a freelancer. My stack includes React, Next.js, TypeScript, Tailwind, Prisma and MongoDB. I'm open to full-time opportunities.",
+    icon: React.createElement(FaReact),
+    date: "2021 - present",
+  },
+] as const;
+
+export const projectsData = [
+  {
+    title: "Valwax",
+    description:
+      "I worked as a full-stack developer on this startup project for 2 years. Users can give public feedback to companies.",
+    tags: ["React", "Next.js", "TypeScript", "i18n", "React Hook Form"],
+    imageUrl: valwaxImg,
+  },
+  {
+    title: "Your pet",
+    description:
+      "Using React, Redux, and related technologies, this web app is a haven for pet lovers. It enables users to post ads, register, customize themes, manage favorites, search, and apply filters. Plus, users can maintain personal profiles to update info and oversee their ads.",
+    tags: [
+      "React",
+      "Redux Toolkit",
+      "Formik",
+      "React Router",
+      "Styled Components",
+    ],
+    imageUrl: yourPetImg,
+  },
+  {
+    title: "Remote demining",
+    description:
+      "Interactive platform for Remote Demining awareness. Features sliders, info cards, news, and admin section for content management.",
+    tags: ["React", "Next.js", "TypeScript", "React Hook Form", "React Slick"],
+    imageUrl: remoteDeminingImg,
+  },
+] as const;
+
+export const skillsData = [
+  "HTML",
+  "CSS",
+  "JavaScript",
+  "TypeScript",
+  "React",
+  "Next.js",
+  "Node.js",
+  "Git",
+  "Tailwind",
+  "MongoDB",
+  "Redux",
+  "Express",
+  "Framer Motion",
+] as const;
