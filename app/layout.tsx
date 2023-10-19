d226164ff5bc3f1f45c47b00bf38d0156a2dede1
ViraMesha <wira.mescha@gmail.com>
2023-10-19 23:06:42 +0300

    feat: create intro section

--- app/layout.tsx
+++ app/layout.tsx
@@ -18,7 +18,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${inter.className} bg-sky-50 text-sky-950 relative h-[5000px]`}
+        className={`${inter.className} bg-sky-50 text-sky-950 relative h-[5000px] pt-28 sm:pt-36`}
       >
         <div className="bg-[#96cef3] absolute top-[-6rem] right-[11rem] -z-10 h-[31.25rem] w-[31.25rem] rounded-full blur-[10rem] sm:w-[68.25rem]"></div>
         <div className="bg-[#c5c5e4] absolute top-[-1rem] left-[-35rem] -z-10 h-[31.25rem] w-[50rem] rounded-full blur-[10rem] sm:w-[68.25rem] md:left-[-33rem] lg:left-[-28rem] xl:left-[-15rem] 2xl:left-[-5rem]"></div>
---
+++ components/Intro.tsx
@@ -0,0 +1,99 @@
+"use client";
+import Image from "next/image";
+import avatarImg from "@/public/images/avatar.jpg";
+import { motion } from "framer-motion";
+import Link from "next/link";
+import { BsArrowRight, BsLinkedin } from "react-icons/bs";
+import { HiDownload } from "react-icons/hi";
+import { FaGithubSquare } from "react-icons/fa";
+export default function Intro() {
+  return (
+    <section className="mb-28 max-w-[50rem] text-center sm:mb-0">
+      <div className="flex items-center justify-center">
+        <div className="relative">
+          <motion.div
+            initial={{ opacity: 0, scale: 0 }}
+            animate={{ opacity: 1, scale: 1 }}
+            transition={{ type: "tween", duration: 0.2 }}
+          >
+            <Image
+              src={avatarImg}
+              alt="Vira Mesha's portrait"
+              width={150}
+              height={150}
+              quality="90"
+              priority
+              className="h-24 w-24 object-cover rounded-full border-[0.35rem] border-white shadow-xl"
+            />
+          </motion.div>
+          <motion.span
+            className="text-4xl absolute bottom-0 right-0"
+            initial={{ opacity: 0, scale: 0 }}
+            animate={{ opacity: 1, scale: 1 }}
+            transition={{
+              type: "spring",
+              stiffness: 125,
+              delay: 0.1,
+              duration: 0.7,
+            }}
+          >
+            👋
+          </motion.span>
+        </div>
+      </div>
+      <motion.p
+        className="mb-10 mt-4 px-4 text-2xl font-medium !leading-[1.5] sm:text-4xl"
+        initial={{ opacity: 0, y: 100 }}
+        animate={{ opacity: 1, y: 0 }}
+      >
+        <span className="font-bold">Hello, my name is Vira! </span>
+        I'm a <span className="font-bold">full-stack developer </span>with{" "}
+        <span className="font-bold">experience</span> working on{" "}
+        <span className="font-bold"> team projects.</span> I have a passion for{" "}
+        <span className="italic"> web development</span> and a solid foundation
+        in <span className="underline"> front-end technologies</span>.
+      </motion.p>
+
+      <motion.div
+        className="flex flex-col sm:flex-row items-center justify-center gap-2 px-4 text-lg font-medium"
+        initial={{ opacity: 0, y: 100 }}
+        animate={{ opacity: 1, y: 0 }}
+        transition={{ delay: 0.1 }}
+      >
+        <Link
+          href="#contact"
+          className="group bg-sky-900 text-white px-7 py-3 flex items-center gap-2 rounded-full outline-none focus:scale-110 hover:scale-110 hover:bg-sky-950 active:scale-105 transition"
+        >
+          Contact me here{" "}
+          <BsArrowRight className="opacity-70 group-hover:translate-x-1 transition" />
+        </Link>
+        <a
+          href="/CV.pdf"
+          target="_blank"
+          className="group bg-white px-7 py-3 flex items-center gap-2 rounded-full outline-none focus:scale-110 hover:scale-110  active:scale-105 transition border border-black/10"
+        >
+          Download CV
+          <HiDownload className="opacity-60 group-hover:translate-y-1 transition" />
+        </a>
+        <a
+          href="http://linkedin.com/in/vira-mesha"
+          aria-label="Link to Vira's Linkedin"
+          target="_blank"
+          rel="noreferrer noopener"
+          className="bg-white text-sky-900 text-[1.35rem] p-3 flex items-center gap-2 rounded-full focus:scale-110 hover:scale-110 hover:text-sky-950  active:scale-105 transition border border-black/10"
+        >
+          <BsLinkedin />
+        </a>
+        <a
+          href="https://github.com/ViraMesha"
+          aria-label="Link to Vira's Github"
+          target="_blank"
+          rel="noreferrer noopener"
+          className="bg-white text-sky-900 text-[1.35rem] p-3 flex items-center gap-2 rounded-full focus:scale-110 hover:scale-110 hover:text-sky-950 active:scale-105 transition border border-black/10"
+        >
+          <FaGithubSquare />
+        </a>
+      </motion.div>
+    </section>
+  );
+}
